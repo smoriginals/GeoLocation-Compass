9d5ae1e3b2e956b9f6d76a62cd50a86f347350c6
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-11-02 20:59:21 +0530

    Update
    NoINFO

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 let arrow = document.querySelector(".compass-arrow");
-let ring = document.querySelector(".compass-ring");
 let speed = document.getElementById('speed');
 
 navigator.geolocation.watchPosition((data) => {
     speed.textContent = `${data.coords.speed !== null ? data.coords.speed : 'N/A'} Km/h`;
     let heading = data.coords.heading !== null ? data.coords.heading : 0;
     arrow.style.transform = `rotate(${heading}deg)`;
-    ring.style.transform = `rotate(${heading}deg)`;
 }, (err) => {
     console.error(err);
     alert("Please Allow The Location Permission");
-},
-    {
-        enableHighAccuracy: true,
-        maximumAge: 0,
-        timeout: 5000
-    }
-);
+});
